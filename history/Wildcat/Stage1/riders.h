b37054f94074655c193ecf114781fe5398c3be2d
control <lzj@hbike.net>
2019-03-28 13:05:52 +0800

    1.新增野猫赛Wildcat目录，并增加第一场西湖小野猫赛参赛车手信息；
    提交人：雷周骏
    审核人：无

---
+++ history/Wildcat/Stage1/riders.h
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  riders.h
+ *
+ *    Description:  用户名单预置表
+ *
+ *        Version:  1.0
+ *        Created:  2018年12月17日 12时40分52秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include "global.h"
+#define _PATH_START		"./autoresult_nespi_001.txt"
+#define _PATH_END		"./end_result_superpi_002.txt"
+
+HIBPRiderInfo riders[] = {
+    {1,     "梅雨龙                   ", "假日单车山地队         ", OPEN},
+    {2,     "雷星                     ", "HangZhou Giant Racing  ", OPEN},
+    {3,     "周吉强                   ", "假日单车山地队         ", OPEN},
+    {4,     "ASH/阿什                 ", "HIBP                   ", OPEN},
+    {5,     "俞斌                     ", "鬼魅仔车队             ", OPEN},
+    {6,     "金鑫越                   ", "上虞梦之队             ", OPEN},
+    {7,     "孙祺龙                   ", "假日单车山地队         ", OPEN},
+    {8,     "Devon Patrick Du Plessis ", "HIBP                   ", OPEN},
+    {9,     "宗朝贤                   ", "soco极限单车           ", OPEN},
+    {10,    "戴一唯                   ", "soco极限单车           ", OPEN},
+    {11,    "黄家琦                   ", "阿里天驱               ", OPEN},
+    {12,    "张佳莉                   ", "鬼魅仔车队             ", OPEN},
+    {13,    "宋舒波                   ", "假日单车山地队         ", OPEN},
+    {14,    "陆成涛                   ", "soco极限单车           ", OPEN},
+    {15,    "陈曦                     ", "阿里天驱               ", OPEN},
+    {16,    "陈一栋                   ", "HIBP                   ", OPEN},
+    {17,    "金也                     ", "soco极限单车           ", OPEN},
+    {18,    "喻杰                     ", "HangZhouGiant          ", OPEN},
+    {19,    "邓浩翔                   ", "假日单车山地队         ", OPEN},
+    {20,    "胡振宇                   ", "假日单车山地队         ", OPEN},
+};
